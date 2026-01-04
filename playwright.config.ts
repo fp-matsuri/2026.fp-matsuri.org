dc09447bb826149d7a321c3bd82ed0f1e2752cbe
Yoshitaka Totsuka <y047aka@gmail.com>
2026-01-05 01:18:00 +0900

    feat: install playwright

---
+++ playwright.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+
+  use: {
+    baseURL: "http://localhost:1234",
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "Google Chrome",
+      use: {
+        ...devices["Desktop Chrome"],
+        channel: "chrome",
+        viewport: { width: 1440, height: 900 },
+      },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"], viewport: { width: 375, height: 812 } },
+    },
+  ],
+
+  webServer: {
+    command: "cd app && gleam run -m lustre/dev start",
+    url: "http://localhost:1234",
+    reuseExistingServer: !process.env.CI,
+  },
+});
